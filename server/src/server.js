02b04ac92b00195e363076b4286f65c39b32a154
Jagroop Singh <sjagroop212@gmail.com>
2024-02-11 11:39:05 -0600

    + Middleware for parsing & CORS

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,32 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
-app.get("/", (req, res) => {
+/* Configuring Cross-Origin Resource Sharing (CORS)*/
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+
+/* These lines of code are configuring the middleware for parsing incoming request bodies in different
+formats. */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+/* Root URL handler */
+app.get('/', (req, res) => {
   console.log(`[GET /] - ${new Date().toISOString()}`);
-  res.send("Hello World!");
+  res.send('Hello World!');
 });
 
+/* This code is responsible for starting the server and listening on a specific port. */
 const PORT = process.env.PORT || 9191;
 app.listen(PORT, () => console.log(`[Server] - Running on port ${PORT}`));
