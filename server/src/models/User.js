05eaf5e7bf004df0a0d93a456bd08459be73b3d1
Jagroop Singh <sjagroop212@gmail.com>
2024-02-19 18:30:05 -0600

    Add Family, Items, and User models

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,15 @@
-// User schema
+require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  userName: { type: String },
+  photoURL: { type: String },
+  family: {
+    type: Schema.Types.ObjectId,
+    ref: 'Family',
+  },
+  isFamilyHead: { type: Boolean, default: false },
+});
+
+const UserModel = mongoose.model('User', userSchema);
