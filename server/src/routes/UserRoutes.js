fc47c99a89b27ba2b3b95af35b86907f784e8c38
Jagroop Singh <sjagroop212@gmail.com>
2024-03-18 22:07:07 -0600

    Add UserRoutes and update index.js

---
+++ server/src/routes/UserRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const {
+  createUser,
+  getUser,
+  deleteUser,
+  updateUser,
+} = require('../controllers/UserController');
+const authMiddleware = require('../middlewares/auth');
+const validate = require('../middlewares/validate');
+const userSchema = require('../validation/ValUser');
+const router = express.Router();
+
+router.get('/getuser', validate(userSchema.getUser), authMiddleware, getUser);
+router.delete(
+  '/:userId',
+  validate(userSchema.deleteUser),
+  // authMiddleware,
+  deleteUser
+);
+
+router.post(
+  '/createuser',
+  validate(userSchema.createUser),
+  // authMiddleware,
+  createUser
+);
+
+router.put(
+  '/updateuser/:userId',
+  validate(userSchema.updateUser),
+  // authMiddleware,
+  updateUser
+);
+
+router.get('/testinguser', authMiddleware, (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = router;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 //Import Family User and Recipe routes
-
+const userRoutes = require('./UserRoutes');
 const router = express.Router();
 
 //Add routes
+router.use('/user', userRoutes);
 
 module.exports = router;
