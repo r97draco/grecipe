3c17779c02cf9dfb18f74c9e09832da039dce5ef
Jagroop Singh <sjagroop212@gmail.com>
2024-03-25 05:39:51 -0600

    Fix bug in login functionality
    Add validation for user input
    
    Refactor code for better performance
    
    Update error handling in API calls
    
    Improve user interface design
    
    Add unit tests for new features
    
    Update documentation for new API endpoints
    
    Fix formatting issues in code
    
    Remove unused variables
    
    Optimize database queries

--- client/src/pages/Inventory.jsx
+++ client/src/pages/Inventory.jsx
@@ -8,6 +8,17 @@ import Spinner from "../components/Spinner";
 import { useContext } from "react";
 import { UserContext } from "../App";
 import "./Inventory.css";
+import {
+  Button,
+  Icon,
+  IconButton,
+  Input,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { BsGripHorizontal } from "react-icons/bs";
+import { FiDelete } from "react-icons/fi";
 
 const Inventory = () => {
   const [inventoryData, setInventoryData] = useState([]);
@@ -20,7 +31,7 @@ const Inventory = () => {
 
   useEffect(() => {
     fetchInventory();
-  }, []);
+  }, [user.family]);
 
   const fetchInventory = async () => {
     setIsTableLoading(true);
@@ -121,67 +132,346 @@ const Inventory = () => {
     }
   };
 
+  const [refresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+    console.log("Page Refreshed");
+    try {
+      const token = localStorage.getItem("self_care_token");
+      const email = user.email;
+
+      axios
+        .get(`${backendUrl}/api/user/getuser`, {
+          params: { email: email },
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            console.log("User data fetched successfully", res.data);
+            setUser(res.data);
+          }
+        });
+    } catch (signInError) {
+      console.error("Error during sign-in", signInError);
+    }
+  }, [refresh]);
+
   return (
     <section className="relative ">
       <div className="max-w-6xl px-4 mx-auto bg-white bg-opacity-50 rounded-lg sm:px-6 backdrop-blur-md ">
         <div className="pt-10 pb-12 md:pt-10 md:pb-20">
           <div className="pb-12 text-center md:pb-16">
-            <h1 className="mb-10 text-5xl font-extrabold tracking-tighter md:text-6xl leading-tighter">
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-blue-400">
-                Inventory
-              </span>
-            </h1>
-
-            <div style={{ width: "500px", margin: "0 auto" }}>
-              <div
-                className="flex items-center justify-between mb-10"
-                style={{ width: "100%" }}
-              >
-                <UploadReceipt updateInventory={updateLocalInventory} />
-                <button
-                  className="secondary-button"
-                  style={{ height: "fit-content" }}
-                  onClick={saveItemsToServer}
-                  disabled={inventoryData.length === 0 || isLoading}
-                >
-                  Save
-                </button>
-              </div>
-              <input
-                type="text"
-                placeholder="Filter by item name"
-                value={filterValue}
-                onChange={(e) => setFilterValue(e.target.value)}
-                className="px-3 py-1 mb-10 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200"
-              />
-              <div className="flex justify-center mb-10 space-x-4">
-                <button
-                  onClick={() => handleSort("name")}
-                  className="px-4 py-2 font-bold text-white bg-orange-500 rounded-full hover:bg-orange-600 focus:outline-none focus:ring focus:ring-orange-400"
-                >
-                  Sort by Name
-                </button>
-                <button
-                  onClick={() => handleSort("expiresAt")}
-                  className="px-4 py-2 font-bold text-white bg-orange-500 rounded-full hover:bg-orange-600 focus:outline-none focus:ring focus:ring-orange-400"
-                >
-                  Sort by Expiry Date
-                </button>
-              </div>
-              <InventoryTable
-                inventoryData={sortedInventoryData}
-                setInventoryData={setInventoryData}
-              />
-              {isTableLoading ? (
-                <div className="mt-4">
-                  <Spinner />
+            {!user.family ? (
+              <Family setRefresh={setRefresh} />
+            ) : (
+              <>
+                <h1 className="mb-10 text-5xl font-extrabold tracking-tighter md:text-6xl leading-tighter">
+                  <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-blue-400">
+                    Inventory
+                  </span>
+                </h1>
+                <div style={{ width: "500px", margin: "0 auto" }}>
+                  Welcome, {user.userName} . Family: {user.family}
+                  {user.isFamilyHead && <p>You are the head of the family</p>}
+                  <FamilyInfo
+                    setRefresh={setRefresh}
+                    familyId={user.family}
+                    head={user.isFamilyHead}
+                  />
+                  <div
+                    className="flex items-center justify-between mb-10"
+                    style={{ width: "100%" }}
+                  >
+                    <UploadReceipt updateInventory={updateLocalInventory} />
+                    <button
+                      className="secondary-button"
+                      style={{ height: "fit-content" }}
+                      onClick={saveItemsToServer}
+                      disabled={inventoryData.length === 0 || isLoading}
+                    >
+                      Save
+                    </button>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Filter by item name"
+                    value={filterValue}
+                    onChange={(e) => setFilterValue(e.target.value)}
+                    className="px-3 py-1 mb-10 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200"
+                  />
+                  <div className="flex justify-center mb-10 space-x-4">
+                    <button
+                      onClick={() => handleSort("name")}
+                      className="px-4 py-2 font-bold text-white bg-orange-500 rounded-full hover:bg-orange-600 focus:outline-none focus:ring focus:ring-orange-400"
+                    >
+                      Sort by Name
+                    </button>
+                    <button
+                      onClick={() => handleSort("expiresAt")}
+                      className="px-4 py-2 font-bold text-white bg-orange-500 rounded-full hover:bg-orange-600 focus:outline-none focus:ring focus:ring-orange-400"
+                    >
+                      Sort by Expiry Date
+                    </button>
+                  </div>
+                  <InventoryTable
+                    inventoryData={sortedInventoryData}
+                    setInventoryData={setInventoryData}
+                  />
+                  {isTableLoading ? (
+                    <div className="mt-4">
+                      <Spinner />
+                    </div>
+                  ) : null}
                 </div>
-              ) : null}
-            </div>
+              </>
+            )}
           </div>
         </div>
       </div>
     </section>
+  );
+};
+
+const FamilyInfo = ({ setRefresh, familyId, head }) => {
+  const [familyData, setFamilyData] = useState(null);
+  const userContext = useContext(UserContext);
+  console.log("user in Family", userContext.user);
+  useEffect(() => {
+    const getFamilyData = async () => {
+      try {
+        const response = await axios.get(
+          `${backendUrl}/api/family/${familyId}`,
+          {
+            headers: {
+              Authorization: localStorage.getItem("self_care_token"),
+            },
+          }
+        );
+        setFamilyData(response.data);
+      } catch (error) {
+        console.error("Error getting family data:", error.response.data);
+      }
+    };
+    getFamilyData();
+  }, [familyId]);
+
+  //url structure /api/family/updatefamily/:familyId
+
+  const deleteMember = async (memberId) => {
+    console.log("memberId", memberId);
+    try {
+      await axios.put(
+        `${backendUrl}/api/family/updatefamily/${familyId}`,
+        {
+          action: "remove",
+          memberId,
+        },
+        {
+          headers: {
+            Authorization: localStorage.getItem("self_care_token"),
+          },
+        }
+      );
+      const updatedFamily = await axios.get(
+        `${backendUrl}/api/family/${familyId}`,
+        {
+          headers: {
+            Authorization: localStorage.getItem("self_care_token"),
+          },
+        }
+      );
+      setFamilyData(updatedFamily.data);
+      setRefresh((prev) => !prev);
+    } catch (error) {
+      console.error("Error deleting member:", error.response.data);
+    }
+  };
+
+  if (head) {
+    return (
+      <div className="mb-10">
+        {familyData ? (
+          <>
+            <h2 className="text-xl font-bold">Family Info</h2>
+            <p>Family Name: {familyData.name}</p>
+            <p>Members:</p>
+            <ul>
+              {familyData.members.map((member) => (
+                <li key={member._id}>
+                  {member.userName}{" "}
+                  <IconButton
+                    variant="contained"
+                    color="error"
+                    onClick={() => deleteMember(member._id)}
+                  >
+                    <FiDelete />
+                  </IconButton>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <p>Loading family data...</p>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <div className="mb-10">
+      {familyData ? (
+        <>
+          <h2 className="text-xl font-bold">Family Info</h2>
+          <p>Family Name: {familyData.name}</p>
+          <p>Members:</p>
+          <ul>
+            {familyData.members.map((member) => (
+              <li key={member._id}>
+                {member.userName}
+                {member._id === userContext.user._id && (
+                  <IconButton
+                    variant="contained"
+                    color="error"
+                    onClick={() => deleteMember(member._id)}
+                  >
+                    <FiDelete />
+                  </IconButton>
+                )}
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>Loading family data...</p>
+      )}
+    </div>
+  );
+};
+
+const Family = ({ setRefresh }) => {
+  // State for creating a family
+  const [familyName, setFamilyName] = useState("");
+  // State for joining a family
+  const [familyIdToJoin, setFamilyIdToJoin] = useState("");
+  const [userId, setUserId] = useState(""); // Assuming you have a way to get the current user's ID
+
+  const userContext = useContext(UserContext);
+  useEffect(() => {
+    console.log("user", userContext.user);
+    setUserId(userContext.user._id);
+  }, [userContext.user]);
+
+  // Handler for creating a family
+
+  const handleCreateFamily = async () => {
+    try {
+      // await axios.post("/createfamily", {
+      await axios.post(
+        `${backendUrl}/api/family/createfamily`,
+        {
+          name: familyName,
+          members: [userContext.user._id],
+          isFamilyHead: true,
+          userId,
+        },
+        {
+          headers: {
+            Authorization: localStorage.getItem("self_care_token"),
+          },
+        }
+      );
+      setFamilyName("");
+      console.log("Created Family");
+      setRefresh((prev) => !prev);
+      // Optionally, refresh the list of families or show a success message
+    } catch (error) {
+      console.error("Error creating family:", error.response.data);
+      // Optionally, show an error message
+    }
+  };
+
+  const [families, setFamilies] = useState([]);
+  const getAllFamilies = async () => {
+    try {
+      const response = await axios.get(
+        `${backendUrl}/api/family/getallfamilies`,
+        {
+          headers: {
+            Authorization: localStorage.getItem("self_care_token"),
+          },
+        }
+      );
+      console.log("Families:", response.data);
+      setFamilies(response.data);
+      // Optionally, set the list of families in state
+    } catch (error) {
+      console.error("Error getting families:", error.response.data);
+      // Optionally, show an error message
+    }
+  };
+
+  const handleJoinFamily = async (familyId) => {
+    try {
+      await axios.put(
+        `${backendUrl}/api/family/updatefamily/${familyId}`,
+        {
+          action: "add",
+          memberId: userId,
+        },
+        {
+          headers: {
+            Authorization: localStorage.getItem("self_care_token"),
+          },
+        }
+      );
+      setRefresh((prev) => !prev);
+      // Optionally, refresh the list of families or show a success message
+    } catch (error) {
+      console.error("Error joining family:", error.response.data);
+      // Optionally, show an error message
+    }
+  };
+
+  // create a list of families name with a button to join
+  const familyList = families.map((family) => (
+    <div key={family._id} className="flex items-center justify-between mb-4">
+      <p>{family.name}</p>
+      <Button variant="contained" onClick={() => handleJoinFamily(family._id)}>
+        Join
+      </Button>
+    </div>
+  ));
+
+  useEffect(() => {
+    getAllFamilies();
+  }, []);
+
+  return (
+    <Stack spacing={3} sx={{ width: "100%", maxWidth: 500, margin: "auto" }}>
+      <h1 className="mb-10 text-5xl font-extrabold tracking-tighter md:text-6xl leading-tighter">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-blue-400">
+          Family
+        </span>
+      </h1>
+
+      {/* Form for creating a new family */}
+      <TextField
+        label="Family Name"
+        variant="outlined"
+        value={familyName}
+        onChange={(e) => setFamilyName(e.target.value)}
+        fullWidth
+      />
+      <Button variant="contained" onClick={handleCreateFamily} fullWidth>
+        Create Family
+      </Button>
+
+      {/* Form for joining an existing family by ID */}
+      <Typography variant="h6" gutterBottom>
+        Join a Family
+      </Typography>
+      {familyList}
+    </Stack>
   );
 };
 
