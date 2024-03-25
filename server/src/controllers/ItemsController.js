cf7b391618741935b2d93df5831d941ad1b6baa1
Jagroop Singh <sjagroop212@gmail.com>
2024-03-25 11:36:18 -0600

    Add getItemsByFamily function and fix createItems function

--- server/src/controllers/ItemsController.js
+++ server/src/controllers/ItemsController.js
@@ -1,6 +1,18 @@
 const GroceryItemService = require('../services/ItemService');
 const UserService = require('../services/UserService');
 
+const getItemsByFamily = async (req, res, next) => {
+  console.log('req.query', req.query);
+  console.log('getItemsByFamily hit in ItemsController');
+  try {
+    const items = await GroceryItemService.getItemsByFamily(req.query.familyId);
+    res.status(200).json(items);
+  } catch (err) {
+    next(err);
+  }
+};
+
+//Need to fix this function
 const createItem = async (req, res, next) => {
   try {
     const item = await GroceryItemService.createItem(req.body);
@@ -12,11 +24,12 @@ const createItem = async (req, res, next) => {
 
 const createItems = async (req, res, next) => {
   try {
-    let user = await UserService.getUserByEmail(req.headers.email);
+    let user = await UserService.getUserByEmail(req.query.email);
 
+    console.log('req.body', req.body);
     const itemObjects = req.body.items.map((item) => ({
       ...item,
-      userId: user._id,
+      familyId: user.family,
     }));
 
     const items = await GroceryItemService.createItems(itemObjects);
@@ -29,9 +42,9 @@ const createItems = async (req, res, next) => {
 
 const getItemsByUser = async (req, res, next) => {
   try {
-    const user = await UserService.getUserByEmail(req.user.email);
+    const user = await UserService.getUserByEmail(req.query.email);
 
-    const items = await GroceryItemService.getItemsByUser(user._id);
+    const items = await GroceryItemService.getItemsByUser(user.family);
     res.status(200).json(items);
   } catch (err) {
     next(err);
@@ -62,6 +75,7 @@ module.exports = {
   createItem,
   createItems,
   getItemsByUser,
+  getItemsByFamily,
   updateItem,
   deleteItem,
 };
