5e7ff85907c5a31569ee0d291ff84ed7473c71a1
mvasylyev <mykhaylo.vasylyev@hotmail.com>
2024-03-25 22:37:26 -0600

    Replaced images for Upload Receipt, Manage Inventory and Family on the Home Page

--- client/src/components/Work.js
+++ client/src/components/Work.js
@@ -1,22 +1,22 @@
 import React from "react";
-import PickMeals from "../Assets/pick-meals-image.png";
-import ChooseMeals from "../Assets/choose-image.png";
-import DeliveryMeals from "../Assets/delivery-image.png";
+import UploadReceipt from "../Assets/upload-receipt.jpg";
+import ManageInventory from "../Assets/manage-inventory.jpg";
+import ManageFamily from "../Assets/manage-family.jpg";
 
 const Work = () => {
   const workInfoData = [
     {
-      image: PickMeals,
+      image: UploadReceipt,
       title: "Upload Grocery Receipt",
       text: "Family head uploads a grocery receipt, and the app extracts item details, updating the inventory.",
     },
     {
-      image: ChooseMeals,
+      image: ManageInventory,
       title: "Manage Inventory",
       text: "View, add, delete, or modify items in the inventory. Track spending and set budget limits.",
     },
     {
-      image: DeliveryMeals,
+      image: ManageFamily,
       title: "Manage Family Members",
       text: "Family head creates/manage member accounts, assigns permissions, and tracks grocery spending.",
     },
