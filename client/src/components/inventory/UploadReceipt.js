02168d3f53252e118d528487dcfddb1e507ec9d9
Jagroop Singh <sjagroop212@gmail.com>
2024-03-27 03:29:30 -0600

    Add file upload functionality to UploadReceipt component

--- client/src/components/inventory/UploadReceipt.js
+++ client/src/components/inventory/UploadReceipt.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import customFetch from "../../utils/fetchWrapper";
 import { notify } from "../Nav";
+import { Button } from "@mui/material";
 
 const UploadReceipt = ({ updateInventory }) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -32,14 +33,23 @@ const UploadReceipt = ({ updateInventory }) => {
   return (
     <div className="flex flex-col space-y-1 font-mono text-left">
       <label
-        htmlFor="receipt-upload"
-        className="block text-sm font-medium text-gray-700"
+        for="inputField"
+        className="px-10 py-5 mr-4 text-sm font-semibold text-black transition-colors duration-200 bg-orange-500 border-0 rounded-full cursor-pointer hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 disabled:bg-gray-300"
       >
-        Upload receipt
+        Upload Receipt
       </label>
       <input
+        type="file"
+        id="inputField"
+        className="hidden"
+        accept=".jpg, .jpeg, .png"
+        onChange={uploadFile}
+        disabled={isLoading}
+      />
+
+      <input
         id="receipt-upload"
-        className="cursor-pointer file:mr-4 file:py-2 file:px-10 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-orange-500 file:text-black file:transition-colors file:duration-200 hover:file:bg-orange-400 focus:file:outline-none focus:file:ring-2 focus:file:ring-offset-2 focus:file:ring-orange-500 disabled:bg-gray-300"
+        className="hidden cursor-pointer file:mr-4 file:py-5 file:px-10 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-orange-500 file:text-black file:transition-colors file:duration-200 hover:file:bg-orange-400 focus:file:outline-none focus:file:ring-2 focus:file:ring-offset-2 focus:file:ring-orange-500 disabled:bg-gray-300"
         type="file"
         accept=".jpg, .jpeg, .png"
         onChange={uploadFile}
